82f4ec66fee12581214c940f674a5bd3519e5431
ngranzow <nate.granzow@gmail.com>
2022-05-08 17:54:37 -0500

    converted app to pwa

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-const path = require("path");
-const webpack = require("webpack");
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
 
 module.exports = {
     entry: {
@@ -22,10 +23,10 @@ module.exports = {
                         loader: 'file-loader',
                         options: {
                             esModule: false,
-                            name (file) {
+                            name(file) {
                                 return "[path][name].[ext["
                             },
-                            publicPath: function(url) {
+                            publicPath: function (url) {
                                 return url.replace("../", "/assets/")
                             }
                         }
@@ -44,6 +45,21 @@ module.exports = {
         }),
         new BundleAnalyzerPlugin({
             analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        }),
+        new WebpackPwaManifest({
+            name: "Food Event",
+            short_name: "Foodies",
+            description: "An app that allows you to view upcoming food events.",
+            start_url: "../index.html",
+            background_color: "#01579b",
+            theme_color: "#ffffff",
+            fingerprints: false,
+            inject: false,
+            icons: [{
+                src: path.resolve("assets/img/icons/icon-512x512.png"),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join("assets", "icons")
+            }]
         })
     ],
     mode: 'development',
