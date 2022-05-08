b160775d4774d44da4d17bd6098189de74cf7c6f
ngranzow <nate.granzow@gmail.com>
2022-05-08 16:12:40 -0500

    added offline functionality:

--- webpack.config.js
+++ webpack.config.js
@@ -46,5 +46,11 @@ module.exports = {
             analyzerMode: "static", // the report outputs to an HTML file in the dist folder
         })
     ],
-    mode: 'development'
+    mode: 'development',
+    devServer: {
+        static: {
+            directory: path.join(__dirname + "/")
+        },
+        port: 8080
+    }
 };
